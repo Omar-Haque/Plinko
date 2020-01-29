0a0c84e16cc3d29d73429ec0a25d9b3e9f8667db
WitchesMe <54588062+WitchesMe@users.noreply.github.com>
2020-01-29 19:05:41 +0530

    Update boundary.js

--- js/boundary.js
+++ js/boundary.js
@@ -1,6 +1,9 @@
 class Boundary{
     constructor(x,y,width,height){
-        this.body = Bodies.rectangle(x,y,width,height, {isStatic:true});
+        var options = {
+            isStatic: true,
+        }
+        this.body = Bodies.rectangle(x,y,width,height, options);
         this.width = width;
         this.height = height;
         World.add(world, this.body);
