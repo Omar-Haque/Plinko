1d9fc73981179a2ec11ad3e407a41f1543b91c68
WitchesMe <54588062+WitchesMe@users.noreply.github.com>
2020-01-29 19:06:51 +0530

    Update peg.js

--- js/peg.js
+++ js/peg.js
@@ -1,17 +1,20 @@
 class Peg{
     constructor(x,y,radius){
         var options = {
-            isStatic: true
+            isStatic: true,
+            restitution: 1.0
         }
         this.body = Bodies.circle(x,y,radius,options);
+        this.hue = random(240);
         this.radius = radius;
         World.add(world,this.body);
     }
     display(){
         var pos = this.body.position;
         push();
+        colorMode(HSB);
         translate(pos.x,pos.y);
-        fill(127);
+        fill(this.hue, 255, 255);
         noStroke();
         ellipse(0,0,this.radius*2);
         pop();
